9b6b82b384a174fbea2472ef37f567fe3578036c
hafiz9977 <hafizuddin.yusof@gmail.com>
2017-02-25 23:55:54 +0800

    Index file

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WatchUs</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div id="links">
+Hello
+</div>
+
+
+<script>
+function loadlink(){
+    $('#links').load('test.php',function () {
+         $(this).unwrap();
+    });
+}
+
+loadlink(); // This will run on page load
+setInterval(function(){
+    loadlink() // this will run after every 3 seconds
+}, 3000);
+</script>
+
+
+</body>
+</html>
